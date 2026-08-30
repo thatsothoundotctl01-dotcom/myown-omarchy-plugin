761b2aae53f261fa0394d7e01d1663d1b57874f6
thatsothoun <thatsothounctrl01@gmail.com>
2026-08-30 23:49:23 +0700

    key bindings shortcut omarchy

---
+++ key_bindings/bindings.lua
@@ -0,0 +1,19 @@
+o.bind("CTRL + ALT + SHIFT + R", "Screenshot", "omarchy-capture-screenshot")
+
+o.bind("SUPER + RETURN", "Terminal", "foot")
+o.bind("SUPER + SHIFT + RETURN", "Browser", "firefox")
+o.bind("SUPER + SHIFT + N", "Neovim", "foot -e nvim")
+o.bind("SUPER + SHIFT + F", "File Manager", "thunar")
+
+o.bind("SUPER + SHIFT + V", "VS Code", "code")
+o.bind("SUPER + SHIFT + G", "GitHub", "firefox https://github.com")
+
+o.bind("SUPER + CTRL + T", "System Monitor", "foot -e btop")
+o.bind("SUPER + CTRL + Q", "Calculator", "qalculate-gtk")
+
+o.bind("SUPER + CTRL + L", "Lock Screen", "hyprlock")
+
+o.bind("SUPER + SHIFT + B", "Bluetooth", "omarchy-menu bluetooth")
+o.bind("SUPER + SHIFT + W", "WiFi", "omarchy-menu wifi")
+
+o.bind("SUPER + CTRL + E", "Emoji Picker", "omarchy-menu emoji")
